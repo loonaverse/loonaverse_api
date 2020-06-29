fe2f5da070ed88868c90ceb97f0390a61142b8a1
veeru153 <32760644+veeru153@users.noreply.github.com>
2020-06-29 22:43:54 +0530

    Merged LTVSchema and LTV, Created ProfileSchema

--- models/ltv.js
+++ models/ltv.js
@@ -1,7 +1,63 @@
 const mongoose = require('mongoose');
 const db = mongoose.connection
-const ltvSchemaData = require('./ltvSchema');
-const ltvSchema = new mongoose.Schema(ltvSchemaData);
+const ltvSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true,
+    },
+    contentId: {
+        type: String,
+        required: true,
+    },
+    videoId: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    era: {
+        type: String,
+        required: true,
+    },
+    members: {
+        type: String,
+        required: true,
+    },
+    blurred: {
+        type: String,
+        required: true,
+    },
+    songs: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    summary: {
+        type: String,
+        required: true,
+    },
+    views: {
+        type: Number,
+        required: true,
+    },
+    likes: {
+        type: Number,
+        required: true,
+    },
+    comments: {
+        type: Number,
+        required: true,
+    },
+});
 
 const LTV = {};
 const ERAs = []
---
+++ models/profile.js
@@ -0,0 +1,100 @@
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+const profileSchema = new mongoose.Schema({
+    stage: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    reveal: {
+        type: String,
+        required: true,
+    },
+    debut: {
+        type: String,
+        required: true,
+    },
+    bday: {
+        type: String,
+        required: true,
+    },
+    birthPlace: {
+        type: String,
+        required: true,
+    },
+    bloodGrp: {
+        type: String,
+        required: true,
+    },
+    traineeDuration: {
+        type: String,
+        required: true,
+    },
+    nextGirlImg: {
+        type: String,
+        required: true,
+    },
+    img: {
+        type: String,
+        required: true,
+    },
+    sign: {
+        type: String,
+        required: true,
+    },
+    memNo: {
+        type: String,
+        required: true,
+    },
+    animal: {
+        type: String,
+        required: true,
+    },
+    color: {
+        type: Object,
+        required: true,
+    },
+    givenName: {
+        type: String,
+        required: true,
+    },
+    hashtags: {
+        type: String,
+        required: true,
+    },
+    teamPosition: {
+        type: String,
+        required: true,
+    },
+    charmPoint: {
+        type: String,
+        required: true,
+    },
+    goals: {
+        type: String,
+        required: true,
+    },
+    msgToOrbit: {
+        type: String,
+        required: true,
+    },
+    mv: {
+        type: Array,
+        required: true,
+    },
+    wd: {
+        type: Array,
+        required: true,
+    },
+    tv: {
+        type: Array,
+        required: true,
+    },
+});
+
+const Profile = mongoose.model('Profile', profileSchema, profile)
+
+module.exports.profile = Profile;
